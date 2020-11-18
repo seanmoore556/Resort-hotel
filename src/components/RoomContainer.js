f07ff11ba6122b48b4fc170bd760230538beb836
Sean Moore <seanmoore556@gmail.com>
2020-11-18 12:11:32 -0500

    Add files via upload

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,45 @@
+import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <>
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </>
+  );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+// another option of writing what is above
+
+// export default function RoomsContainer() {
+//   return (
+//     <RoomConsumer>
+//       {(value) => {
+//         const { loading, sortedRooms, rooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+
+//         return (
+//           <div>
+//             room container
+//             <RoomFilter rooms={rooms} />
+//             <RoomList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// }
